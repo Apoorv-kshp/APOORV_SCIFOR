864f17c6c6dbdeb87665bf01f86ca1daf99aba23
Apoorv Kashyap <104822652+Apoorv-kshp@users.noreply.github.com>
2024-06-04 16:02:59 +0530

    Create Functions Q-3.py

---
+++ PYTHON/Homework/Functions Q-3.py
@@ -0,0 +1,16 @@
+# Function to Find the Maximum of Three Numbers
+
+def max_of_three(a, b, c):
+    """Function to find the maximum of three numbers."""
+    return max(a, b, c)
+
+# Example usage
+num1 = 10
+num2 = 14
+num3 = 12
+result = max_of_three(num1, num2, num3)
+print(f"The maximum of {num1}, {num2}, and {num3} is {result}.")
+
+# Output:
+
+The maximum of 10, 14, and 12 is 14.
